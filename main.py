c26aa0452b621084f5b0d2c69bb089f25a8e9127
Tyler Weed <tylerjweed@gmail.com>
2026-03-09 18:02:37 -0400

    basic imu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 import smbus
-from mpu6050 import MPU6050
 
 from Vehicle import Vehicle
 
@@ -16,40 +15,37 @@ WHO_AM_I = 0x0F
 CTRL_REG1 = 0x20  # Example: enable XYZ axes, 100Hz
 OUT_X_L = 0x28    # Base register for accelerometer X-axis LSB
 OUT_Y_L = 0x2A    # Base register for Y-axis LSB
-OUT_Z_L = 0x2C    # Base re
+OUT_Z_L = 0x2C    # Base register for Z-axis LSB
 
-i2c_bus = 2
-device_address = 0x37
-freq_divider = 0x04
+# -------------------------------
+# Initialize bus
+# -------------------------------
+bus = smbus.SMBus(BUS_NUMBER)
 
-# Make an MPU6050
-mpu = MPU6050(i2c_bus, device_address, freq_divider)
+# -------------------------------
+# Helper functions
+# -------------------------------
+def read_word(register_l):
+    """Read 16-bit signed value from two registers (little endian)"""
+    lsb = bus.read_byte_data(DEVICE_ADDRESS, register_l)
+    msb = bus.read_byte_data(DEVICE_ADDRESS, register_l + 1)
+    value = (msb << 8) | lsb
+    if value >= 32768:
+        value -= 65536
+    return value
 
-# Initiate your DMP
-mpu.dmp_initialize()
-mpu.set_DMP_enabled(True)
 
-packet_size = mpu.DMP_get_FIFO_packet_size()
-FIFO_buffer = [0]*64
-
-while True: # infinite loop
-    if mpu.isreadyFIFO(packet_size): # Check if FIFO data are ready to use...
-        
-        FIFO_buffer = mpu.get_FIFO_bytes(packet_size) # get all the DMP data here
-        
-        q = mpu.DMP_get_quaternion_int16(FIFO_buffer)
-        grav = mpu.DMP_get_gravity(q)
-        roll_pitch_yaw = mpu.DMP_get_euler_roll_pitch_yaw(q)
-        
-        print('roll: ' + str(roll_pitch_yaw.x))
-        print('pitch: ' + str(roll_pitch_yaw.y))
-        print('yaw: ' + str(roll_pitch_yaw.z))
-        print('\n')
-        
-# Initialization:
-# Create a vehicle object
-
-# Start the IMU process to continually track
+try:
+    i=0
+    while(True):
+        i += 1
+        x = read_word(OUT_X_L)
+        y = read_word(OUT_Y_L)
+        z = read_word(OUT_Z_L)
+        print(f"Sample {i+1} - X: {x}, Y: {y}, Z: {z}")
+        time.sleep(0.5)
+except Exception as e:
+    print("[Test 4] I2C error during continuous read:", e)
 # from mpu6050 import MPU6050
 
 # i2c_bus = 1
