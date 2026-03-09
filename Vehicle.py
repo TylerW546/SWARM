47fd4449b205198565652e88e31860ed4c03911f
Tyler Weed <tylerjweed@gmail.com>
2026-03-09 15:38:22 -0400

    commit for pull testing

--- Vehicle.py
+++ Vehicle.py
@@ -5,3 +5,12 @@ from Motors import *
 class Vehicle:
     def __init__(self):
         self.index
+
+    def start_imu_process(self):
+        pass
+
+    def start_communication_module(self):
+        pass
+
+    def start_device_discovery(self):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import time
+from Vehicle import Vehicle
 
-while(True):
-	print("HELLO")
-	time.sleep(1)
+this_vehicle = Vehicle()
+
+
+this_vehicle.start_imu_process()
+this_vehicle.start_communication_module()
+this_vehicle.start_device_discovery()
+
+while True:
+    time.sleep(1)
+    print("Hello")
+
+# Initialization:
+# Create a vehicle object
+
+# Start the IMU process to continually track
+# Start the communication module
+# Start the device discovery process
