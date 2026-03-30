aacd59e8cb60d730d944089f380e7f7aa3624425
Tyler Weed <tylerjweed@gmail.com>
2026-03-30 13:45:51 -0400

    printing ip to uwb for ssh

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ uwb = UWBInterface(ser)
 
 uwb.assign_id(str(uuid.getnode()))
 
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't have to be reachable
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+local_ip = get_ip()
+print("Local IP:", local_ip)
+uwb.ser.add_to_send_queue(f"*MY_IP={local_ip}~")
+
 class State(Enum):
     INIT_DEVICE_DISCOVERY = 1
     INIT_PARENTING = 2
