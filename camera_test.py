25d93243f5504bec88173129a3c8aef1f8d57b2a
jrohozen <jrohozen@andrew.cmu.edu>
2026-04-13 10:42:17 -0400

    updated color space - LAB

--- camera_test.py
+++ camera_test.py
@@ -3,8 +3,16 @@ import cv2
 import numpy as np
 import time
 
+IMAGE_WIDTH = 400
+IMAGE_HEIGHT = 400
+
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
+config = picam2.create_preview_configuration({"size": (IMAGE_WIDTH, IMAGE_HEIGHT)})
+picam2.configure(config)
+picam2.set_controls({
+    "AeEnable": False,  # Disable auto exposure
+    "AwbEnable": False, # Disable auto white balance
+})
 picam2.start()
 time.sleep(1)
 
@@ -12,18 +20,19 @@ while True:
     frame = picam2.capture_array()
 
     # Convert to HSV
+    lab = cv2.cvtColor(frame, cv2.COLOR_RGB2LAB)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+
+    cv2.circle(frame, center=(IMAGE_WIDTH//2, IMAGE_HEIGHT//2), radius=10, color=(0, 0, 255), thickness=2)
+    print(frame[IMAGE_WIDTH//2][IMAGE_HEIGHT//2])
 
     # For tracking something bright orange
-    HUE = 26
-    lower = np.array([HUE - 5, 25, 25])
-    upper = np.array([HUE + 5, 255, 255])
+    lower = np.array([0, 164, 147])
+    upper = np.array([255, 200, 200])
 
-    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.inRange(lab, lower, upper)
 
-    h, w, _ = frame.shape
-    print(hsv[h//2, w//2])
+    cv2.circle(lab, center=(IMAGE_WIDTH//2, IMAGE_HEIGHT//2), radius=10, color=(0, 0, 255), thickness=2)
 
     # Find centroid
     moments = cv2.moments(mask)
@@ -36,6 +45,7 @@ while True:
         cv2.circle(frame, center=(cx, cy), radius=100, color=(255, 0, 0), thickness=2)
 
     cv2.imshow("frame", frame)
+    cv2.imshow("lab", lab)
     cv2.imshow("mask", mask)
 
     if cv2.waitKey(1) == 27:
