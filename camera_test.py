d6b2b0219687332f765c8eb364a894ac5e106597
Tyler Weed <tylerjweed@gmail.com>
2026-04-17 18:34:11 -0400

    change color bounds

--- camera_test.py
+++ camera_test.py
@@ -24,10 +24,12 @@ while True:
     blurframe = cv2.GaussianBlur(frame, (5,5), 0)
     lab = cv2.cvtColor(blurframe, cv2.COLOR_BGR2LAB)
 
-    lower = np.array([120, 130, 160])
-    upper = np.array([255, 180, 255])
+    lower = np.array([120, 100, 170])
+    upper = np.array([255, 140, 230])
 
-    print("Color at image center: ", lab[IMAGE_HEIGHT//2, IMAGE_WIDTH//2])
+    # print("Color at image center: ", lab[IMAGE_HEIGHT//2, IMAGE_WIDTH//2])
+    # cv2.circle(lab, center=(IMAGE_WIDTH//2, IMAGE_HEIGHT//2), radius=10, color=(0, 0, 255), thickness=2)
+    
 
     mask = cv2.inRange(lab, lower, upper)
 
@@ -45,7 +47,6 @@ while True:
             # draw the center of the circle
             cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
             break
-    cv2.circle(lab, center=(IMAGE_WIDTH//2, IMAGE_HEIGHT//2), radius=10, color=(0, 0, 255), thickness=2)
 
     cv2.imshow("lab", lab)
     cv2.imshow("frame", frame)
