a72267501d8f0697df8aa114ef8560b4bc590a36
Tyler Weed <tylerjweed@gmail.com>
2026-04-30 19:07:22 -0400

    slower turn

--- Util.py
+++ Util.py
@@ -25,7 +25,7 @@ SEEN_FRAMES_THRESHOLD = 2
 FRAME_DURATION = 0.080
 
 MOTOR_SPEED = 40
-TURN_SPEED = 35
+TURN_SPEED = 20
 
 # Wheel encoder to distance values
 WHEEL_CIRC_METERS = 2 * 3.1415 * 0.035 # Circumference in meters
