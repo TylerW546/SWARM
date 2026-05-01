68d255dc40446af276fea45e40a39a8c6cfcf585
Tyler Weed <tylerjweed@gmail.com>
2026-04-30 21:04:33 -0400

    update dists

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ while True:
         v.state = State.WANDER
 
     if v.state == State.WANDER:
+        print(f"Distance to other: {v.dist_to_other}m, uwb says {v.uwb.dist}m")
         if frame_count % 1 == 1:
             if leader:
                 v.uwb.enter_ranging_mode()
