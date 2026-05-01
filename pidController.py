eb314a6add54f1fb856a7616b111df169afdc9d6
Tyler Weed <tylerjweed@gmail.com>
2026-04-30 20:47:45 -0400

    fix converged thresh

--- pidController.py
+++ pidController.py
@@ -223,7 +223,7 @@ class pidController:
 
         self.speed_history.append(self.speeds.copy())
         self.signal_history.append(self.signals.copy())
-        print(f"[SIGNALS]: {self.signals[LEFT_WHEEL]}, {self.signals[RIGHT_WHEEL]}")
+        # print(f"[SIGNALS]: {self.signals[LEFT_WHEEL]}, {self.signals[RIGHT_WHEEL]}")
 
     def rotate_left_update(self):
         if self.state != PID_State.TURNING_LEFT:
